201ebee49d5ede63105e493ab83ed1906bcf7c8c
Tankblank <40700065+Tankblank@users.noreply.github.com>
2019-07-20 00:40:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const ytdl = require('ytdl-core');
 const request = require('request');
-const devs = ['517069424180592656','601883438848409600'];
+const devs = ['601883438848409600','517069424180592656'];
 const child_process = require("child_process");
 const adminprefix = "!";
 const fs = require('fs');
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 
-const yt_api_key = "AIzaSyDSjXGQzPfrBE4UdmJUyL8cTlpHXtMm7o0";
+const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = '$';
 
 client.on('ready', () => {
